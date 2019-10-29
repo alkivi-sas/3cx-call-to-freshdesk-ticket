6ed091c74dbdf9d3063864cc3980db538bd97281
Alexandre Martin <alexandre@alkivi.fr>
2019-10-29 17:43:13 +0100

    project and code cleaning

--- 3cx_to_API.py
+++ 3cx_to_API.py
@@ -3,15 +3,6 @@ import logging
 import os
 import sys
 
-"""URL = "https://alkivi.freshdesk.com/api/v2/contacts?phone=%2B33145729797"
-HEADERS = {"Content-Type": "application/json"}
-
-response = requests.get(url=URL, auth=AUTH, headers=HEADERS)
-logging.warning(response)
-logging.warning(response.content)
-logging.warning(vars(response))"""
-
-
 def main(argv):
     callerNumber = None
     if type(argv) == str and len(argv) == 12:
@@ -40,10 +31,18 @@ def main(argv):
     config.read(config_file)
     domain = config.get('freshdesk', 'domain', fallback='missing conf file')
     api_key = config.get('freshdesk', 'api_key')
-    URL = config.get('freshdesk', 'url')+callerNumber
+    
+    URLPhone = config.get('freshdesk', 'urlPhone')+callerNumber
+    URLMoblie = config.get('freshdesk', 'urlMobile')+callerNumber
+
     HEADERS = {"Content-Type": "application/json"}
-    logging.warning("requesting " + URL)
-    response = requests.get(url=URL, auth=(api_key,''), headers=HEADERS)
+    logging.warning("requesting " + URLPhone)
+    response = requests.get(url=URLPhone, auth=(api_key,''), headers=HEADERS)
     logging.warning(response.content)
+    """ if no contact try with mobile"""
+
+    """if no contact with mobile create a contact"""
+
+    """create a ticket with the contact"""
 
 main(sys.argv[1])
