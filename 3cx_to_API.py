a5edcb42992d706d0c72ddca150a002ebb856238
Alexandre Martin <alexandre@alkivi.fr>
2019-10-31 16:10:19 +0100

    code format standars

--- 3cx_to_API.py
+++ 3cx_to_API.py
@@ -1,12 +1,10 @@
-import requests
 import logging
 import os
 import sys
-import json
 from freshdesk.api import API
+import configparser
 
 config_file = os.path.dirname(os.path.realpath(__file__))+'/freshdesk.conf'
-import configparser
 config = configparser.RawConfigParser()
 config.read(config_file)
 api_key = config.get('freshdesk', 'api_key')
@@ -15,41 +13,44 @@ domain = config.get('freshdesk', 'domain')
 agent_name = config.get('freshdesk', 'name')
 group_id = config.get('freshdesk', 'group_id')
 
-def main(argv):
-    logging.basicConfig(filename='/var/log/3cx-v2-freshdesk-macos.log',level=logging.DEBUG)
-    
-    checkPhoneFormat(argv)
+
+def main(phone_number: str) -> None:
+    logging.basicConfig(filename='/var/log/3cx-v2-freshdesk-macos.log',
+            level=logging.DEBUG)
+
+    check_phone_format(phone_number)
     api = API(domain, api_key, version=2)
 
-    getContact(argv, api)
+    get_contact(phone_number, api)
 
 
-def getContact(argv, api):
-    contacts = api.contacts.list_contacts(phone=argv)
+def get_contact(phone_number: str, api: API) -> None:
+    contacts = api.contacts.list_contacts(phone=phone_number)
 
     if len(contacts) < 1:
-        logging.info("no phone known for " + argv)
-        
+        logging.info("no phone known for " + phone_number)
+
         logging.info("trying mobile")
-        contacts = api.contacts.list_contacts(mobile=argv)
+        contacts = api.contacts.list_contacts(mobile=phone_number)
         if len(contacts) < 1:
-            logging.info("no mobile known for " + argv)
+            logging.info("no mobile known for " + phone_number)
 
             logging.info("creating new contact")
-            
-            newContactTicket(argv, api)
+
+            new_contact_ticket(phone_number, api)
         else:
             logging.info("creating ticket for " + contacts[0].name)
-            
-            newTicket(argv, api, contacts[0])
+
+            new_ticket(api, contacts[0])
     else:
         logging.info("creating ticket for " + contacts[0].name)
 
-        newTicket(argv, api, contacts[0])
+        new_ticket(api, contacts[0])
 
 
-def newTicket(argv, api, contact):
-    ticket = api.tickets.create_ticket("Support call between " + contact.name + " and " + agent_name,
+def new_ticket(api: API, contact: Contact) -> None:
+    ticket = api.tickets.create_ticket(
+            "Support call between " + contact.name + " and " + agent_name,
             description="Support call between " + contact.name + " and " + agent_name,
             requester_id=contact.id,
             group_id=int(group_id),
@@ -62,10 +63,11 @@ def newTicket(argv, api, contact):
     logging.info(ticket)
 
 
-def newContactTicket(argv, api):
-    ticket = api.tickets.create_ticket("Support call between " + argv + " and " + agent_name,
-            description="Support call between " + argv + " and " + agent_name,
-            phone=argv,
+def new_contact_ticket(phone_number: str, api: API) -> None:
+    ticket = api.tickets.create_ticket(
+            "Support call between " + phone_number + " and " + agent_name,
+            description="Support call between " + phone_number + " and " + agent_name,
+            phone=phone_number,
             group_id=int(group_id),
             responder_id=int(agent_id),
             priority=1,
@@ -75,24 +77,24 @@ def newContactTicket(argv, api):
     logging.info(ticket)
 
 
-def checkPhoneFormat(argv):
-    callerNumber = None
-    if type(argv) == str and len(argv) == 12:
-        callerNumber = argv
+def check_phone_format(phone_number: str) -> None:
+    caller_number = None
+    if type(phone_number) == str and len(phone_number) < 13 and len(phone_number) > 10:
+        caller_number = phone_number
 
         """removing the +"""
-        if argv[0] == '+':
-            callerNumber = callerNumber.replace('+', '')
+        if caller_number[0] == '+':
+            caller_number = caller_number.replace('+', '')
         else:
-            logging.warning("wrong format, should be +33000000000")
-            logging.warning("entry is "+ argv)
+            logging.warning("wrong format, should be +00000000000")
+            logging.warning("entry is " + phone_number)
             sys.exit()
 
         """checking if it is all numbers"""
-        for number in range(len(callerNumber)):
-            if not callerNumber[number].isdigit():
-                logging.warning("wrong format, should be +33000000000")
-                logging.warning("entry is "+ argv)
+        for number in range(len(caller_number)):
+            if not caller_number[number].isdigit():
+                logging.warning("wrong format, should be +00000000000")
+                logging.warning("entry is " + phone_number)
                 sys.exit()
 
 
